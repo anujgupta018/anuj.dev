d77126371e2d173390ab493d8671df4f6b0da549
Anuj <invincibleanuj1718@gmail.com>
2025-02-04 18:51:59 +0530

    Fixed another errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Github, Linkedin, Code } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import profile from "../public/profile.jpg";
