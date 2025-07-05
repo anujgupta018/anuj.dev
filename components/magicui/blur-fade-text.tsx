4892a760da4896820db70ab39f39d9d43d9a65f7
Anuj <invincibleanuj1718@gmail.com>
2025-07-05 18:07:13 +0530

    Fixed bugs

--- components/magicui/blur-fade-text.tsx
+++ components/magicui/blur-fade-text.tsx
@@ -45,7 +45,7 @@ const BlurFadeText = ({
               exit="hidden"
               variants={combinedVariants}
               transition={{
-                yoyo: Infinity,
+                repeat: Infinity,
                 delay: delay + i * characterDelay,
                 ease: "easeOut",
               }}
@@ -69,7 +69,6 @@ const BlurFadeText = ({
           exit="hidden"
           variants={combinedVariants}
           transition={{
-            yoyo: Infinity,
             delay,
             ease: "easeOut",
           }}
